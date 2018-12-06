0b392918e7bfaa8d01c76136cc0d7101598b73c1
ollebollebagare <olof.malmgren@live.se>
2018-12-06 12:00:32 +0100

    First version of Queue added

--- Inlupp2/Queue.h
+++ Inlupp2/Queue.h
@@ -1,1 +1,52 @@
 #pragma once
+#include <cstddef>
+
+
+template <class datatype>
+class Queue
+{
+private:
+	struct Node
+	{
+		datatype Element;
+		struct Node * Next;
+	};
+	struct Node * First;
+	struct Node * Last;
+	int Count;
+
+public:
+	void AddElement(datatype NewElement) {
+		struct Node * NewNode = new struct Node;
+		NewNode->Element = NewElement;
+		NewNode->Next = NULL;
+		Count++;
+		if (First == NULL)
+			First = NewNode;
+		else
+		{
+			Last->Next = NewNode;
+		}
+		Last = NewNode;
+
+	}
+	bool RemoveElement()
+	{
+		if (First == NULL) return false;
+		Node * temp = First;
+		First = First->Next;
+		delete temp;
+		Count--;
+		return true;
+	}
+	int ReturnCount()
+	{
+		return Count;
+	}
+	Queue() {
+		Count = 0;
+		First = NULL;
+		Last = NULL;
+	}
+
+};
