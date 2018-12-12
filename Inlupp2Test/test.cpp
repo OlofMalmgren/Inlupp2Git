1e871389853afc5da0269d1e599dce826f8e19de
robelMike <robelMike@users.noreply.github.com>
2018-12-12 11:08:32 +0100

    Stacklist test

--- Inlupp2Test/test.cpp
+++ Inlupp2Test/test.cpp
@@ -1,6 +1,21 @@
-#include "pch.h"
+//#include "pch.h"
+//#include <gtest/gtest.h>
+//#include "c:\Users\robel\Documents\GitHub\Inlupp2Git\Inlupp2\Stack.h"
+//
+//
+//TEST(VectorStackListTest, PushFunctionShouldFillTheStack)
+//{
+//	StackV<int> stacklist;
+//	stacklist.push(4);
+//	EXPECT_EQ(4, stacklist.push);
+//
+//}
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+//TEST(VectorStackListTest, PushFunctionShouldFillTheStack)
+//{
+//	int hej = 1;
+//	int lol = 2;
+//	hej = hej + lol;
+//	EXPECT_EQ(3, hej);
+//	
+//}
